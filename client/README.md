68c47c23b8abfcac28b53495ca63ae24bec543b3
imonem <amr.nabeel@gmail.com>
2022-06-28 23:54:37 +0200

    Added New Project feature

--- client/README.md
+++ client/README.md
@@ -1,3 +1,9 @@
+# Project Management Using MERN Stack
+
+## WiP
+
+### Incomplete Readme.md
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import Clients from './components/Clients';
-import AddClientModal from './components/AddClientModal';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Project from './pages/Project';
 
 //warning https://stackoverflow.com/questions/66121208/defining-custom-merge-function-to-resolve-inmemorycache-merge-in-graphql
 const cache = new InMemoryCache({
@@ -32,11 +34,16 @@ function App() {
 	return (
 		<>
 			<ApolloProvider client={client}>
-				<Header />
-				<div className='container'>
-					<AddClientModal />
-					<Clients />
-				</div>
+				<Router>
+					<Header />
+					<div className='container'>
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/projects/:id' element={<Project />} />
+							<Route path='*' element={<NotFound />} />
+						</Routes>
+					</div>
+				</Router>
 			</ApolloProvider>
 		</>
 	);
